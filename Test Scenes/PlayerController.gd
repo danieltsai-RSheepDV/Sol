d0e43f0853018ae638cf944d29c6a50eff209ecd
Daniel Tsai <32005886+danieltsai-RSheepDV@users.noreply.github.com>
2024-01-27 14:00:13 -0800

    Input lag issues

--- Test Scenes/PlayerController.gd
+++ Test Scenes/PlayerController.gd
@@ -1,27 +1,39 @@
 extends CharacterBody3D
 
+const SMALL_NUMBER = 0.1
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const speed = 8.0
+const accel = 4
+const decel = 4
+const velPower = 1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+# Internal Variables
+var targetVelocity = Vector3(0, 0, 0)
+
+func _process(delta):
+	# Get movement input
+	var input_dir = Input.get_vector("Left", "Right", "Up", "Down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		targetVelocity = direction * speed
+	else:
+		targetVelocity = Vector3.ZERO
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("Left", "Right", "Up", "Down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	# Update Velocity
+	var xDif = targetVelocity.x - velocity.x
+	var xAccel = accel if (abs(velocity.x) > SMALL_NUMBER) else decel
+	velocity.x += delta * pow(abs(xDif) * xAccel, velPower) * sign(xDif)
+	
+	var zDif = targetVelocity.z - velocity.z
+	var zAccel = accel if (abs(velocity.z) > SMALL_NUMBER) else decel
+	velocity.z += delta * pow(abs(zDif) * zAccel, velPower) * sign(zDif)
 
 	move_and_slide()
